ccb8269f05b996a24bab007f317ca8b1334a8b9c
My-kull <hurst-maxims.0m@icloud.com>
2026-01-25 17:08:20 +0200

    Project Starter

---
+++ WebProject/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Header from "./components/header";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
