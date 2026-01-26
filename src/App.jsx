4dcaecc4142f3c492e0b49f8466f501fdfd02778
My-kull <hurst-maxims.0m@icloud.com>
2026-01-26 10:47:33 +0200

    Fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
 import Header from "./components/header";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
