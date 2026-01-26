7621acc19d741a10bdc73256b130d6ec88330774
My-kull <hurst-maxims.0m@icloud.com>
2026-01-26 11:08:39 +0200

    Fixed Header, added ready to use css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,25 +3,23 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div>
-        <h1>My App</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/services">Services</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <h1>My App</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
