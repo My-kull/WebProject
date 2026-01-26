9542a3a0d56dbd852dbfdf3637909b40c428f8a3
My-kull <hurst-maxims.0m@icloud.com>
2026-01-26 10:47:33 +0200

    Fixed App.jsx

--- WebProject/src/components/header.jsx
+++ WebProject/src/components/header.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-function Header() {
-  return <header>Website</header>;
-}
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>My App</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
