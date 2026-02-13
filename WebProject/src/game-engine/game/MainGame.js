77dfe72ef21050953c7920ad2ddf251e51b5a490
MightySeveri <severi.kerkka@gmail.com>
2026-02-13 13:40:13 +0200

    type fix

--- WebProject/src/game-engine/game/MainGame.js
+++ WebProject/src/game-engine/game/MainGame.js
@@ -1,10 +1,6 @@
 import { Engine } from "../Engine.js";
 
-/**
- * Game entry point wrapper.
- * Keep this file game-specific, and keep Engine reusable.
- */
-export function bootGame(canvas) {
+ bootGame(canvas) {
   const engine = new Engine(canvas);
   engine.start();
   return engine;
