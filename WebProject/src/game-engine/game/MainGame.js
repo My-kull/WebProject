b4a1bdc54187ceb3b2ef00636dbf5aa777957208
MightySeveri <severi.kerkka@gmail.com>
2026-03-04 23:18:03 +0000

    Blacksmith camp v1 release

--- WebProject/src/game-engine/game/MainGame.js
+++ WebProject/src/game-engine/game/MainGame.js
@@ -1,7 +1,7 @@
 import { Engine } from "../Engine.js";
 
-export function bootGame(canvas) {
-  const engine = new Engine(canvas);
+export function bootGame(canvas, options = {}) {
+  const engine = new Engine(canvas, options);
   engine.start();
   return engine;
 }
