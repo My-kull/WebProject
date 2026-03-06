ccbba05a7c14bb866af82c58de54b3ef06f7a79b
My-kull <hurst-maxims.0m@icloud.com>
2026-03-06 10:33:46 +0200

    [feat: API V1] Crud Operations Functional

--- CM3/cm3/backend/controllers/vehicleRentalControllers.js
+++ CM3/cm3/backend/controllers/vehicleRentalControllers.js
@@ -1,29 +1,52 @@
-const VehicleRental = require('../models/vehicleRentalModel');
-const mongoose = require('mongoose');
+const VehicleRental = require("../models/vehicleRentalModel");
+const mongoose = require("mongoose");
 
 // GET /api/vehicleRentals
 const getAllVehicleRentals = async (req, res) => {
-  res.send("getAllVehicleRentals");
+  const vehicleRentals = await VehicleRental.find({});
+  res.json(vehicleRentals);
 };
 
 // POST /api/vehicleRentals
 const createVehicleRental = async (req, res) => {
-  res.send("createVehicleRental");
+  const vehicleRental = new VehicleRental(req.body);
+  const savedVehicleRental = await vehicleRental.save();
+  res.status(201).json(savedVehicleRental);
 };
 
 // GET /api/vehicleRentals/:vehicleRentalId
 const getVehicleRentalById = async (req, res) => {
-  res.send("getVehicleRentalById");
+  const vehicleRental = await VehicleRental.findById(
+    req.params.vehicleRentalId,
+  );
+  if (!vehicleRental) {
+    return res.status(404).json({ error: "Vehicle rental not found" });
+  }
+  res.json(vehicleRental);
 };
 
 // PUT /api/vehicleRentals/:vehicleRentalId
 const updateVehicleRental = async (req, res) => {
-  res.send("updateVehicleRental");
+  const vehicleRental = await VehicleRental.findByIdAndUpdate(
+    req.params.vehicleRentalId,
+    req.body,
+    { new: true, runValidators: true },
+  );
+  if (!vehicleRental) {
+    return res.status(404).json({ error: "Vehicle rental not found" });
+  }
+  res.json(vehicleRental);
 };
 
 // DELETE /api/vehicleRentals/:vehicleRentalId
 const deleteVehicleRental = async (req, res) => {
-  res.send("deleteVehicleRental");
+  const vehicleRental = await VehicleRental.findByIdAndDelete(
+    req.params.vehicleRentalId,
+  );
+  if (!vehicleRental) {
+    return res.status(404).json({ error: "Vehicle rental not found" });
+  }
+  res.status(204).end();
 };
 
 module.exports = {
